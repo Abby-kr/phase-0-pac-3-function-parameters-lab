2c81105ac0452eef107457f7744da4727ad305f2
Abby-kr <abby.a.otieno@gmail.com>
2023-07-15 01:03:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function introduction(name){
 function introductionWithLanguage(name,language){
     return "Hi,my name is ${name} and I am learning to program in ${language}."
 }
-function introductionWithLanguageOptional(name = Gracie,language = JavaScript){
+function introductionWithLanguageOptional(name,language = JavaScript){
     return "Hi,my name is ${name} and I'm learning to program in ${language}."
 }
